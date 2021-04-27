b1d598343551c0f04718f158e36563e777d0a174
btuffreau <benjamin.tuffreau@gmail.com>
2021-04-27 12:22:19 +0200

    removing shell scripts

--- start_az.sh
+++
@@ -1,3 +0,0 @@
-az vm start -n kafka-broker-0 -g kafka_lab
-az vm start -n kafka-broker-1 -g kafka_lab
-az vm start -n kafka-broker-2 -g kafka_lab
--- stop_az.sh
+++
@@ -1,7 +0,0 @@
-az vm stop -n kafka-broker-0 -g kafka_lab
-az vm stop -n kafka-broker-1 -g kafka_lab
-az vm stop -n kafka-broker-2 -g kafka_lab
-az vm deallocate -n kafka-broker-0 -g kafka_lab
-az vm deallocate -n kafka-broker-1 -g kafka_lab
-az vm deallocate -n kafka-broker-2 -g kafka_lab
-
